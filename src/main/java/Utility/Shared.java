0bd5bebb4d3c77307a87074a75107019aa320224
Azmain Hossain <azmainhossain@azmains-mbp.lan>
2019-11-07 16:28:47 -0500

    first try

---
+++ src/main/java/Utility/Shared.java
@@ -0,0 +1,67 @@
+package Utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Shared {
+	static Properties prop;
+	public static Properties readPropertyFile(String pathname)	{
+		
+				try {
+			FileInputStream f=new FileInputStream(new File(pathname));
+		
+	} 
+				catch (FileNotFoundException e) {
+			 
+			e.printStackTrace();
+			System.out.println("File Not Found");
+			
+	}
+		
+					 prop= new Properties();
+		
+						prop.getProperty(pathname);
+		
+		
+		return prop;
+	
+	}
+	
+	public static void openBrowser(WebDriver driver)	{
+		
+		if(prop.get("browser").equals("chrome")) {
+			
+			driver= new ChromeDriver();
+		}
+		
+		else if (prop.get("browser").equals("FireFox")) {
+			driver= new FirefoxDriver();
+		}
+		
+		else if (prop.get("browser").equals("Internet Explorer")) {
+			driver= new InternetExplorerDriver();
+		}
+		
+		else if (prop.get("browser").equals("Safari")) {
+			driver= new SafariDriver();
+		}
+		
+		else {
+				
+				System.out.println("Driver is not found");
+			
+		}
+			
+		
+		
+	}
+	
+}
